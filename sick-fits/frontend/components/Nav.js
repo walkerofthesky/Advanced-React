7c6ba62f4138c6aa9b192f81ed9262eaf7fc86cd
Dustin Parker <dustin.parker@vivintsolar.com>
2018-11-01 07:06:43 -0600

    Introduction and CSS & Styled Components
    Completed first 2 modules

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import NavStyes from './styles/NavStyles';
+
+const Nav = () => (
+  <NavStyes>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyes>
+);
+
+export default Nav;
