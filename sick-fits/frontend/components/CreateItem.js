225bf76219427d562d5b592f0cbcd1a1a109f999
Dustin Parker <dustin.parker@vivintsolar.com>
2019-02-01 16:41:57 -0700

    Uploading Images
    Video #18

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -39,6 +39,26 @@ class CreateItem extends Component {
     });
   };
 
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/adustinparker/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    );
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     const { title, description, image, largeImage, price } = this.state;
 
@@ -60,6 +80,19 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <img width="200" src={image} alt="Upload preview" />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
