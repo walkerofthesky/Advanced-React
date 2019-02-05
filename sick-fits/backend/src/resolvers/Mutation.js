2b23847791e76f2fb20566eb516a43c90cb03229
Dustin Parker <dustin.parker@vivintsolar.com>
2019-02-05 09:04:08 -0700

    Deleting Items
    Video #20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -26,6 +26,14 @@ const Mutations = {
       },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // TODO: 2. Check if they own that item, or have the permissions
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
--- sick-fits/frontend/components/item.js
+++ sick-fits/frontend/components/item.js
@@ -7,6 +7,7 @@ import formatMoney from '../lib/formatMoney';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   static propTypes = {
@@ -41,7 +42,7 @@ class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add to cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
