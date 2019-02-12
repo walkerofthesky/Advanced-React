f64e9fa0f2ec75790a076cd4b38c93454434c090
Dustin Parker <dustin.parker@vivintsolar.com>
2019-02-12 16:42:52 -0700

    Sending Email
    Video #31

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -104,7 +105,18 @@ const Mutations = {
       data: { resetToken, resetTokenExpiry }
     });
     // 3. Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: 'adustinparker@gmail.com',
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${
+        process.env.FRONTEND_URL
+      }/reset?resetToken=${resetToken}">Click Here to Reset</a>`)
+    });
 
+    // 4. return the message
     return { message: 'Thanks!' };
   },
   async resetPassword(parent, args, ctx, info) {
