952380300e12280f953c9b0ba33bde0592822e63
Dustin Parker <dustin.parker@vivintsolar.com>
2019-02-15 09:34:17 -0700

    Apollo Local State Queries and Mutations
    Video #39

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
+import { Mutation } from 'react-apollo';
 import Link from 'next/link';
 import NavStyes from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import { TOGGLE_CART_MUTATION } from './Cart';
 
 const Nav = () => (
   <User>
@@ -23,6 +25,9 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </Fragment>
         )}
         {!me && (
